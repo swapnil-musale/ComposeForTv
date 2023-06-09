7d2e33e2c498b7ee6d8f40a19358c0cd23aa476d
Swapnil Musale <swapnil.musale@mutualmobile.com>
2023-06-10 02:40:09 +0530

    1. Configuration Cache Enabled 2. Refactored files

--- app/src/main/java/com/example/composefortv/MainActivity.kt
+++ app/src/main/java/com/example/composefortv/MainActivity.kt
@@ -3,17 +3,20 @@ package com.example.composefortv
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.core.view.WindowCompat
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Surface
+import androidx.tv.material3.Text
 import com.example.composefortv.ui.theme.ComposeForTvTheme
 
+@OptIn(ExperimentalTvMaterial3Api::class)
 class MainActivity : ComponentActivity() {
 
-    @OptIn(ExperimentalTvMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -23,7 +26,12 @@ class MainActivity : ComponentActivity() {
                     shape = RectangleShape,
                     modifier = Modifier.fillMaxSize(),
                 ) {
-                    App()
+                    Box(
+                        modifier = Modifier.fillMaxSize(),
+                        contentAlignment = Alignment.Center,
+                    ) {
+                        Text(text = "Hello World !")
+                    }
                 }
             }
         }
