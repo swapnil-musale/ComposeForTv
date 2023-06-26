fa160cba15eb8417e4fdeedc228957ce0b772f69
Swapnil Musale <swapnil.musale@mutualmobile.com>
2023-06-26 11:44:05 +0530

    Splash Screen API Implementation

--- app/src/main/java/com/example/composefortv/MainActivity.kt
+++ app/src/main/java/com/example/composefortv/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.core.view.WindowCompat
 import androidx.tv.material3.ExperimentalTvMaterial3Api
 import androidx.tv.material3.Surface
@@ -17,6 +18,7 @@ import com.example.composefortv.ui.theme.ComposeForTvTheme
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen()
         super.onCreate(savedInstanceState)
         WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent {
